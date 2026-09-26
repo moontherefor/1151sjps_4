8afd9e8480b2b13516f3ac104cc915742da102bd
moontherefor <moontherefor@gmail.com>
2026-09-26 20:12:18 +0800

    新增測試檔案

--- 測試腳本.gd
+++ 測試腳本.gd
@@ -1,11 +1,7 @@
-extends Node2D
-# 載入測試腳本
-var 測試腳本 = preload("res://測試腳本.gd").new()
+# 這個腳本已經在.gitignore裡被排除，不會同步到github
 
 # _ready()會在節點準備好時執行
 func _ready() -> void:
-	# 執行測試腳本裡的函數：測試腳本.函數名稱
-	
 	pass # Replace with function body.
 
 
